5ff4f2a0657ef0dd205d20fa6bb4af8465b95b37
Rachid BEN SAID <20190072@d12-06.etudiants.campus.villejuif>
2019-10-09 14:54:23 +0200

    remove main

--- main.js
+++
@@ -1,7 +0,0 @@
-const uppercase = require('./ex33')
-
-console.log(uppercase("4534"))
-console.log(uppercase("458.6"))
-console.log(uppercase("-458.6"))
-console.log(uppercase("1.9E+2"))
-console.log(uppercase("one"))
